10d6863a7c28de7cfe58f72cd29d6217b416237d
jeanny.benon <jeannybbenon@gmail.com>
2026-06-01 15:44:20 +0200

    page recherche.php

--- recherche.php
+++ recherche.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'config.php';
-include 'header.php';
 
-?>
-<head>
-<h1>Recherche</h1>;
-</head>
-    
 
-<?php
-include'footer.php';
+$recherche = isset($_GET['q']) ? trim ($_GET['q']) : '';
 
+$livres =[];
+
+if (!empty($recherche)) {
+    $stmt = $pdo->prepare("SELECT id, titre, prix FROM livre WHERE titre LIKE ? LIMIT 5").
+    $stmt-> execute (["%$recherche%"]);
+    $livres = $stmt->fetchAll(PDO::FETCH_ASSO);
+}
+
+header ('Content-Type : application/json');
+echo json_encode($livres);
+exit;
