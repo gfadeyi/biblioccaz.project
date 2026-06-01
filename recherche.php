232bfd406c6ade507d12582bdcbb7cff0e73a9cf
jeanny.benon <jeannybbenon@gmail.com>
2026-06-01 16:02:57 +0200

    recherche.js

--- recherche.php
+++ recherche.php
@@ -7,7 +7,7 @@ $recherche = isset($_GET['q']) ? trim ($_GET['q']) : '';
 $livres =[];
 
 if (!empty($recherche)) {
-    $stmt = $pdo->prepare("SELECT id, titre, prix FROM livre WHERE titre LIKE ? LIMIT 5").
+    $stmt = $pdo->prepare("SELECT id, titre, prix FROM livre WHERE titre LIKE ? LIMIT 5");
     $stmt-> execute (["%$recherche%"]);
     $livres = $stmt->fetchAll(PDO::FETCH_ASSO);
 }
