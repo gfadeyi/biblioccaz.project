55760302f72724ef18e66cf21b6d8919f78c440a
gfadeyi <g.fadeyi@myskolae.fr>
2026-06-04 22:18:33 +0200

    update pc

--- config.php
+++ config.php
@@ -3,6 +3,19 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+$inactivity_limit = 900; 
+
+if (isset($_SESSION['last_activity'])) {
+    $duration = time() - $_SESSION['last_activity'];
+    if ($duration > $inactivity_limit) {
+        session_unset();
+        session_destroy();
+        header("Location: login.php?timeout=1");
+        exit();
+    }
+}
+$_SESSION['last_activity'] = time();
+
 $isLocal = ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '::1');
 
 $host = 'localhost';
