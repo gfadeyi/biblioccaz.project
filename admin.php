64f1183932d3e2ff068a0b2ea9a238018ab25946
Davidt-lang <davidaime01@gmail.com>
2026-06-06 17:03:19 +0200

    passage de la gestion d'image du  captcha en back

--- admin.php
+++ admin.php
@@ -82,6 +82,9 @@ include 'header.php';
                 <a href="admin_logs.php" class="list-group-item list-group-item-action py-3">
                     <i class="bi bi-journal-text me-2 text-success"></i> Journal d'activité (Logs)
                 </a>
+                <a href="admin_captcha.php" class="list-group-item list-group-item-action py-3">
+    <i class="bi bi-images me-2 text-success"></i> Gestion des images CAPTCHA
+</a>
             </div>
         </div>
     </div>
