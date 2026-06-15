20c06bd3b47e98effacba2a8c43ca0a6b0c5fdbd
jeanny.benon <jeannybbenon@gmail.com>
2026-06-15 12:20:39 +0200

    temps de déco.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$inactivity_limit = 960; 
+$inactivity_limit = 900; 
 
 if (isset($_SESSION['last_activity'])) {
     $duration = time() - $_SESSION['last_activity'];
